44f7abf08096b1b1683987261651d8c28bece45d
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2023-08-28 19:44:43 +1000

    Create hub_output_light_matrix.py

---
+++ python_files/hub_output_light_matrix.py
@@ -0,0 +1,16 @@
+# light_matrix.py
+
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+# --- SETUP
+
+hub = PrimeHub()
+
+
+# --- MAIN LOOP
+while True:
+    
